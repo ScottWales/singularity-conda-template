70166d400281df0144bb3aa0ab67fefa08ac942c
Scott Wales <scott.wales@bom.gov.au>
2023-12-04 15:06:14 +1100

    Squashfs based build

--- build.sh
+++ build.sh
@@ -14,7 +14,13 @@ set -o pipefail
 
 SCRIPT_DIR=${PBS_O_WORKDIR:-$( cd -- "$( dirname -- "$(readlink -f ${BASH_SOURCE[0]})" )" &> /dev/null && pwd )}
 
+NAME=conda
+VERSION=202312
+COMMANDS="python conda"
+
 WORKDIR=${TMPDIR}/squash
+STAGEDIR=/scratch/$PROJECT/$USER/ngm
+
 mkdir -p "$WORKDIR"
 
 PATH=/scratch/hc46/saw562/tmp/container/bom-ngm-conda/bin:$PATH
@@ -27,9 +33,11 @@ if ! [ -f "$WORKDIR/conda.squashfs" ]; then
     conda pack --prefix $WORKDIR/mamba --arcroot bom-ngm/mamba --output $WORKDIR/conda.squashfs --compress-level 0
 fi
 
+# Make a copy for this container
 cp $SCRIPT_DIR/base.sif $WORKDIR/image.sif
 
-apptainer sif add \
+# Add in conda
+singularity sif add \
     --datatype 4 \
     --partfs 1 \
     --parttype 4 \
@@ -37,3 +45,15 @@ apptainer sif add \
     --groupid 1 \
     $WORKDIR/image.sif \
     $WORKDIR/conda.squashfs
+
+# Stage
+mkdir -p $STAGEDIR/apps/$NAME/$VERSION/etc
+cp $WORKDIR/image.sif $STAGEDIR/apps/$NAME/$VERSION/etc/$NAME-$VERSION.sif
+ln -sf $NAME-$VERSION.sif $STAGEDIR/apps/$NAME/$VERSION/etc/image.sif
+
+rm -rf $STAGEDIR/apps/$NAME/$VERSION/bin
+mkdir -p $STAGEDIR/apps/$NAME/$VERSION/bin
+cp imagerun $STAGEDIR/apps/$NAME/$VERSION/bin
+for c in $COMMANDS; do
+    ln -s imagerun $STAGEDIR/apps/$NAME/$VERSION/bin/$c
+done
